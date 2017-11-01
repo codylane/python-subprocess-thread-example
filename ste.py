8df600623171a3e7c0c0481f2b2b4640fba20f35
cody.lane <cody.lane@ocio.usda.gov>
2017-11-01 14:31:55 -0600

    use logging instad of print also small refactor

--- ste.py
+++ ste.py
@@ -1,57 +1,117 @@
 #!/usr/bin/env python
 
-import time
-import threading
-import subprocess
+import exceptions
+import logging
 import os
-import sys
-
 import pytest
+import shlex
+import subprocess
+import sys
+import threading
 
-SLEEP_TIME=sys.argv[1:] or '1'
-TIMEOUT=3
+
+SLEEP_TIME = sys.argv[1:] or '1'
+TIMEOUT = 3
+LOGGER = None
 
 
-def timestamp():
-    return time.strftime('%Y-%m-%dT%H:%M:%S')
+def setup_logging(name=__name__, level=logging.INFO, format='%(asctime)s - %(name)s - %(levelname)s - %(message)s'):  # noqa: E501
+    global LOGGER
+
+    formatter = logging.Formatter(format)
+    logger = logging.getLogger(name)
+    logger.setLevel(level)
+
+    sh = logging.StreamHandler()
+    sh.setFormatter(formatter)
+    sh.setLevel(level)
+
+    logger.addHandler(sh)
+
+    logger.debug('test message')
+
+    LOGGER = logger
+    return logger
+
 
 class Command(object):
+
     def __init__(self, cmd, timeout=3):
-        self.cmd = cmd
+        self.cmd = shlex.split(cmd)
+        self.cmd[0] = self._find_executable(self.cmd[0])
         self.timeout = timeout
         self.p = None
+        self.logger = logging.getLogger(__name__)
+        self.rc = None
 
-    def run(self, timeout):
-        print '{0} Starting thread'.format(timestamp())
-        self.p = subprocess.Popen(self.cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    def run(self, timeout=None):
+        if timeout:
+            self.timeout = timeout
+
+        self.rc = None
+        self.logger.info('Command: %s', self.cmd)
+        self.logger.info('Starting thread')
+        self.p = subprocess.Popen(self.cmd, stdout=subprocess.PIPE, stderr=subprocess.PIPE)  # noqa: E501
+
         def communicate():
             self.stdout, self.stderr = self.p.communicate()
-            print '{0} thread finished'.format(timestamp())
+            self.logger.info('Finished thread')
 
         thread = threading.Thread(target=communicate)
         thread.start()
-        thread.join(timeout)
+        thread.join(self.timeout)
 
-        print 'is_alive={0}'.format(thread.is_alive())
+        self.logger.info('Thread alive: %s', thread.is_alive())
         if thread.is_alive():
-            print '{0} Terminating process'.format(timestamp())
-	    self.p.terminate()
-            print '{0} rc={0}'.format(timestamp(), self.p.returncode)
+            self.logger.info('Terminating process')
+            self.p.terminate()
+            self.logger.info('RC: %s', self.p.returncode)  # noqa: E501
             thread.join()
-	    raise Exception('timer expired')
-	return self.p.returncode
+            raise Exception('Timer expired')
+
+        self.rc = self.p.returncode
+        self.logger.info('Returning RC: %s', self.rc)
+        return self.rc
+
+    def _find_executable(self, executable):
+        path_dirs = os.environ.get('PATH')
+
+        if not path_dirs:
+            raise exceptions.PathEnvironmentVariableNotSetError()
+
+        orig_dir = os.getcwd()
+        for path_dir in path_dirs.split(os.pathsep):
+            if not os.path.exists(path_dir):
+                continue
+
+            os.chdir(path_dir)
+            if os.path.exists(executable):
+                exec_path = os.path.abspath(executable)
+                os.chdir(orig_dir)
+                return exec_path
+
+        os.chdir(orig_dir)
+        raise exceptions.ExecutableNotFoundError(executable)
 
 
 class TestCommand(object):
     def test_command_happy_path(self):
         """Testing happy path that command executes successfully"""
-        cmd = Command(['sleep', '0.1'])
+
+        cmd = Command('sleep 0.1')
         rc = cmd.run(timeout=1)
         assert rc == 0
 
     def test_command_times_out(self):
         """Test sleep 10 will timeout after 1 second and raise Exception"""
+
         with pytest.raises(Exception) as exp:
-            cmd = Command(['sleep', '10'])
+            cmd = Command('sleep 10')
             cmd.run(timeout=1)
-	exp.match('timer expired')
+        exp.match('Timer expired')
+
+
+if __name__ == '__main__':
+    setup_logging(__name__)
+    cmd = Command('sleep 1')
+    print(cmd.run(timeout=3))
